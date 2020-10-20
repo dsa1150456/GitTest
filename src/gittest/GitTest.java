199409677017b147b31b2b296eeaf10299f778d6
Nine <ewq1150456_@hotmail.com>
2020-10-20 17:06:10 +0700

    second

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -16,8 +16,12 @@ public class GitTest {
      */
     public static void main(String[] args) {
         sayHello();
+        sayGoodbye();
     }
     public static void sayHello(){
         System.out.println("Hello");
+    }
+    public static void sayGoodbye(){
+        System.out.println("Goodbye");
     }
 }
